2a8c14cc88c27cc3129ad47692061abc07ecf9f2
bugua123 <47909592@163.com>
2020-03-09 15:50:54 +0800

    收缩索引

--- src/test/java/com/wzw/springbootelasticsearch/Elastic_ShrinkIndex.java
+++ src/test/java/com/wzw/springbootelasticsearch/Elastic_ShrinkIndex.java
@@ -2,13 +2,17 @@ package com.wzw.springbootelasticsearch;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.action.admin.indices.open.OpenIndexRequest;
-import org.elasticsearch.action.admin.indices.open.OpenIndexResponse;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.shrink.ResizeRequest;
+import org.elasticsearch.action.admin.indices.shrink.ResizeResponse;
+import org.elasticsearch.action.support.ActiveShardCount;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CloseIndexRequest;
 import org.elasticsearch.client.indices.CloseIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,11 +22,11 @@ import java.io.IOException;
 
 /**
  * 1、判断索引是否存在
- *    如果存在，关闭索引
+ *    如果存在，收缩索引
  */
 @SpringBootTest
 @Slf4j
-public class Elastic_CloseIndex {
+public class Elastic_ShrinkIndex {
 
 @Autowired
     RestHighLevelClient restHighLevelClient;
@@ -35,8 +39,8 @@ public class Elastic_CloseIndex {
         if (!existsIndex(INDEX_TEST)) {
             System.out.println("索引不存在 ");
         }else {
-            System.out.println("索引存在关闭索引");
-            closeIndex(INDEX_TEST);
+            System.out.println("索引存在收缩索引");
+            shrinkIndex(INDEX_TEST);
 
         }
 
@@ -56,15 +60,31 @@ public class Elastic_CloseIndex {
         return exists;
     }
     /**
-     * 关闭索引
+     * 收缩索引
      * @param index
      * @throws IOException
      */
-    public void closeIndex(String index) throws IOException {
+    public void shrinkIndex(String index) throws IOException {
 
-        CloseIndexRequest request=new CloseIndexRequest(index);
-//        request.timeout(TimeValue.timeValueMillis(2));//可以按照这种样式添加参数
-        CloseIndexResponse closeIndexResponse = restHighLevelClient.indices().close(request, RequestOptions.DEFAULT);
-        System.out.println("关闭索引信息: " + JSON.toJSONString(closeIndexResponse));
+        ResizeRequest request=new ResizeRequest("target_index",index );
+        /**
+         * 设置参数
+         */
+        request.timeout(TimeValue.timeValueMillis(2));
+        request.timeout("2m");
+        request.masterNodeTimeout(TimeValue.timeValueMillis(1));
+        request.masterNodeTimeout("1m");
+        request.setWaitForActiveShards(2);
+        request.setWaitForActiveShards(ActiveShardCount.DEFAULT);
+        request.getTargetIndexRequest().settings(Settings.builder()
+        .put("index.number_of_shards",2)
+        .putNull("index.routing.allocation.require._name")
+        );
+        request.getTargetIndexRequest().alias(new Alias("target_alias"));
+
+        //执行
+        ResizeResponse resizeResponse = restHighLevelClient.indices().shrink(request, RequestOptions.DEFAULT);
+
+        System.out.println("索引收缩信息: " + JSON.toJSONString(resizeResponse));
     }
 }
